ec837014da76edb0d189ef1174ce1db893241c6f
weixin_63557130 <2829025546@qq.com>
2026-07-24 14:25:22 +0800

    matrix_inverse算子的golden代码
    Co-authored-by: dx<2829025546@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !830 merge master into master
    
    matrix_inverse算子的golden代码
    
    Created-by: weixin_63557130
    Commit-by: dx
    Merged-by: cann-robot
    Description: ## 描述
    
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    这个文件实现了 矩阵求逆（Matrix Inverse） 算子，属于 Catlass::golden 命名空间下的 CPU 端黄金参考实现（用于验证 Ascend NPU 上算子结果的正确性）。
    ## 关联的Issue
    
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    https://gitcode.com/cann/catlass/issues/331
    ## 原因
    
    <!--说明此次改动的目的、解决的问题等，应与类型标签匹配 -->
    
    ## 测试
    
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    
    <!--  [x] 表示选中 -->
    
    - [ ] Bug修复
    - [x] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    See merge request: cann/catlass!830

--- examples/common/golden.hpp
+++ examples/common/golden.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2025 Huawei Technologies Co., Ltd.
+ * Copyright (c) 2025-2026 Huawei Technologies Co., Ltd.
  * This program is free software, you can redistribute it and/or modify it under the terms and conditions of
  * CANN Open Software License Agreement Version 2.0 (the "License").
  * Please refer to the License for details. You may not use this file except in compliance with the License.
@@ -15,5 +15,6 @@
 #include "golden/fill_data.hpp"
 #include "golden/matmul.hpp"
 #include "golden/conv2d.hpp"
+#include "golden/matrix_inverse.hpp"
 
 #endif // EXAMPLES_COMMON_GOLDEN_HPP
